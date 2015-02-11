f6503572b468c1ecf27d4599315cba764dd87cd6
Mark deVilliers <markdevilliers@gmail.com>
2015-02-11 19:42:54 +0000

    Updated signalr, json.net to latest 4.5 versions.

---
+++ SignalR.RabbitMQ.Example/App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SignalR.RabbitMQ.Example.App_Start;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace SignalR.RabbitMQ.Example.App_Start
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
